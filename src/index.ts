1d20668364fa856dd65c9bc5e9264028219d0a97
chideraemmanuel <120053997+chideraemmanuel@users.noreply.github.com>
2023-09-02 04:38:53 +0100

    feat: enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './router';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -12,6 +13,9 @@ const port = process.env.PORT || 5000;
 // ENABLE BODY PARSER
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// ENABLE CORS
+app.use(cors());
 
 app.use('/', router());
 
